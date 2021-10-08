63cbc0be279cfe58ef4c1d2a339d1e3e7f07cab3
Giovanni Zenari <zenari12@gmail.com>
2021-10-08 20:43:33 +0200

    Pokemon Species tests definition

---
+++ pokemonController_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetPokemonSpeciesDataValidPokemon(t *testing.T) {
+
+}
+
+func TestGetPokemonSpeciesDataInvalidPokemon(t *testing.T) {
+
+}
+
+func TestGetPokemonSpeciesDataConnectionError(t *testing.T) {
+
+}
