aabd5587c11b8a2c4ab45b4043cb21e04acd08a5
Giovanni Zenari <zenari12@gmail.com>
2021-10-09 23:00:51 +0200

    Translation controller implementation

--- translationController.go
+++ translationController.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-
+	"net/url"
 )
 
+type TranslationResponse struct {
+	Contents ContentsResource
+}
+
+type ContentsResource struct {
+	Translated string
+}
+
 func getTranslatedDescription(appCtx AppCtx, description string, habitat string, isLegendary bool) (string, error) {
-	return "", nil
+
+	translationUrl := getTranslationURL(appCtx, habitat, isLegendary)
+	resp, err := appCtx.client.R().
+		SetResult(&TranslationResponse{}).
+		Get(translationUrl + url.PathEscape(description))
+
+	if err != nil {
+		return description, err
+	}
+	if resp.StatusCode() > 399 {
+		return description, &RequestError{statusCode: resp.StatusCode(), err: err,}
+	}
+
+    return resp.Result().(*TranslationResponse).Contents.Translated, err
 }
 
 func getTranslationURL(appCtx AppCtx, habitat string, isLegendary bool) string {
-	return ""
+	if habitat == "cave" || isLegendary {
+		return "https://api.funtranslations.com/translate/yoda.json?text="
+	}
+	return "https://api.funtranslations.com/translate/shakespeare.json?text="
 }
