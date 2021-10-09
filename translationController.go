bf762353184bf0421fbb40e5723e92699f1b915d
Giovanni Zenari <zenari12@gmail.com>
2021-10-09 21:02:21 +0200

    Translation controller function and test skeleton

---
+++ translationController.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+
+)
+
+func translateDescription(appCtx AppCtx, description string, habitat string, isLegendary bool) string {
+	return ""
+}
+
+func getTranslatedDescription(appCtx AppCtx, url string, description string) string {
+	return ""
+}
+
+func getTranslationURL(appCtx AppCtx, habitat string, isLegendary bool) string {
+	return ""
+}
---
+++ translationController_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetTranslationURL(t *testing.T) {
+
+}
+
+func TestGetTranslatedDescription(t *testing.T) {
+	
+}
+
+func TestTranslateDescription(t *testing.T) {
+	
+}
