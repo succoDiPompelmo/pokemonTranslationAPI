cd49f7abac6a21d80c4718cfd4876cfd33959c68
Giovanni Zenari <zenari12@gmail.com>
2021-10-09 20:25:15 +0200

    Pokemon API basic route implementation

--- routes.go
+++ routes.go
@@ -7,7 +7,19 @@ import (
 func pokemonRoutes(appCtx AppCtx) {
 	
 	appCtx.app.Get("/pokemon/:pokemonName", func(c *fiber.Ctx) error {
-		return nil
+		pokemonName := c.Params("pokemonName")
+		pokemonSpeciesData, err := getPokemonSpeciesData(appCtx, pokemonName)
+
+		if err != nil {
+			return fiber.ErrInternalServerError
+		}
+
+		return c.JSON(fiber.Map{
+			"name": pokemonName,
+			"description": pokemonSpeciesData.getDescription(),
+			"habitat": pokemonSpeciesData.getHabitat(),
+			"Is_legendary": pokemonSpeciesData.isLegendary(),
+		})
 	})
 
 	appCtx.app.Get("/pokemon/translated/:pokemonName", func(c *fiber.Ctx) error {
